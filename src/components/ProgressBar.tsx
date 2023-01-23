ae4a927d05fd995c5cd2c5c114e79a6ee3cfb9a0
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-01-22 21:34:20 -0300

    animations and focus added

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,22 +1,22 @@
-import * as Progress from "@radix-ui/react-progress";
+import * as Progress from '@radix-ui/react-progress'
 
 interface ProgressBarProps {
-  progress: number;
+    progress: number
 }
 
 export function ProgressBar(props: ProgressBarProps) {
-  const progressStyles = {
-    width: `${props.progress}%`,
-  };
+    const progressStyles = {
+        width: `${props.progress}%`
+    }
 
-  return (
-    <Progress.Root className="H-3 rounded-xl bg-zinc-700 w-full mt-4">
-      <Progress.Indicator
-        aria-label="Progress of habits completed that day"
-        aria-value={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
-        style={progressStyles}
-      />
-    </Progress.Root>
-  );
+    return (
+        <Progress.Root className='H-3 rounded-xl bg-zinc-700 w-full mt-4'>
+            <Progress.Indicator
+                aria-label='Progress of habits completed that day'
+                aria-value={props.progress}
+                className='h-3 rounded-xl bg-violet-600 transition-all'
+                style={progressStyles}
+            />
+        </Progress.Root>
+    )
 }
